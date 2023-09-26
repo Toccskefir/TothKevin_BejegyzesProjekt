df215cad6eff3d294e42b3fdc95ab638b4433c6b
Tóth Kevin <tothkevin0716@gmail.com>
2023-09-26 08:59:25 +0200

    2. feladat (a rész megoldása

--- BejegyzesProjekt/Program.cs
+++ BejegyzesProjekt/Program.cs
@@ -10,6 +10,13 @@ namespace BejegyzesProjekt
     {
         static void Main(string[] args)
         {
+            //2. Feladat
+            //a
+            List<Bejegyzes> lista = new List<Bejegyzes>();
+            Bejegyzes bejegyzes1 = new Bejegyzes("Tóth Kevin", "Épp dolgozatot írok, nem leszek elérhető!");
+            Bejegyzes bejegyzes2 = new Bejegyzes("Kiss Sára", "De szép a mai nap!");
+            lista.Add(bejegyzes1);
+            lista.Add(bejegyzes2);
         }
     }
 }
